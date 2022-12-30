4f8d57f97149c02fff863a36775df6cd71fb3d11
silverace_71 <alekshilderbrand@gmail.com>
2022-12-29 17:45:32 -0700

    made this include apt-utils for pi-hole

--- ipy-install.sh
+++ ipy-install.sh
@@ -3,6 +3,7 @@
 echo "do you wish to intsall pouter (y/n)?"
 read confirm
     if [[ $confirm == "y" ]]; then
+        sudo apt install apt-utils -y
         sudo apt-get install -y python3 && sudo curl -sSL https://raw.githubusercontent.com/silverace71/pouter/main/main.py > pouter.py && sudo python3 pouter.py
     else
         echo "certified bruh moment"
