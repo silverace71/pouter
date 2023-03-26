607c28b40dba5ed84997cb7e6dd41e6d18782ae0
Astro <astroorbis@gmail.com>
2023-03-25 22:00:08 -0400

    add pip

--- ipy-install.sh
+++ ipy-install.sh
@@ -4,7 +4,7 @@ echo "do you wish to intsall pouter (y/n)?"
 read confirm
 	if [[ $confirm == "y" ]]; then
 		sudo apt-get update
-		sudo apt install dhcpcd5 apt-utils iptables python3 unbound -y
+		sudo apt install dhcpcd5 apt-utils iptables python3 python3-pip unbound -y
 		python3 -m pip install requests
 		sudo curl -sSL https://raw.githubusercontent.com/silverace71/pouter/main/main.py > pouter.py
 		sudo python3 pouter.py
